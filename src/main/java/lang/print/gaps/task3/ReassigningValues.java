4e858d5bbffb345f962a01e80a87d916001493b0
abbosking <105731575+abbosking@users.noreply.github.com>
2022-10-23 22:51:24 +0500

    Update ReassigningValues.java

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -5,11 +5,20 @@ public class ReassigningValues {
         int first=1;
         int second=10;
         int third=100;
+        Sytem.out.println(first);
+        Sytem.out.println(second);
+        Sytem.out.println(third);
         int linkToFirst=first;
         int linkToSecond=second;
         int linkToThird=third;
         first=15;
         second=6;
         third=4;
+        Sytem.out.println(first);
+        Sytem.out.println(second);
+        Sytem.out.println(third);
+        Sytem.out.println(linkToFirst);
+        Sytem.out.println(linkToSecond);
+        Sytem.out.println(linkToThird);
     }
 }
