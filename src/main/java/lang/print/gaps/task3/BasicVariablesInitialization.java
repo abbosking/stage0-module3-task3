c28f54e684847cef165a0e028e5d0419c68048df
abbosking <105731575+abbosking@users.noreply.github.com>
2022-10-26 11:27:23 +0500

    Update BasicVariablesInitialization.java

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,11 +2,11 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        int first=1;
-        int second=10;
-        int third=100;
-        Sytem.out.println(first);
-        Sytem.out.println(second);
-        Sytem.out.println(third);
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
